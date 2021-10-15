3f6514087c72814e6f7eece7d051054a702ccc22
itzVort3x1 <saikaustubh10@gmail.com>
2021-10-15 18:02:28 +0530

    added navbar for login/signup page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,5 @@
 import React, {useRef, useState}from 'react';
-import { Form, Button, Card, Alert, Container } from 'react-bootstrap';
+import { Form, Button, Card, Alert, Container, Navbar, Nav } from 'react-bootstrap';
 import {useAuth} from '../../context/AuthContext';
 import { Link, useHistory } from 'react-router-dom';
 
@@ -30,33 +30,49 @@ const Login = () => {
      }
 
      return (
+          <>
+          <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
+               <Container>
+                    <Navbar.Brand href="/">Elite-Division</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav" style={{justifyContent: 'end'}}>
+                    <Nav >
+                         <Nav.Link href="/signup">Sign up</Nav.Link>
+                         <Nav.Link eventKey={2} href="/login">
+                         Login
+                         </Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+               </Container>
+          </Navbar>
           <Container className="" style={{ minHeight: '100vh', paddingTop: '200px', paddingLeft: '450px'}}>
-          <div className="w-100" style={{ maxWidth: '400px'}}>     
-          <Card>
-               <Card.Body>
-                    <h2 className="text-center mb-4">Login</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <Form onSubmit={handleSubmit}>
-                         <Form.Group id="email" className="mb-2">
-                              <Form.Label>Email</Form.Label>
-                              <Form.Control id="email" ref={emailRef} type="email" placeholder="Email" required/>
-                         </Form.Group>
-                         <Form.Group id="password" className="mb-4">
-                              <Form.Label>Password</Form.Label>
-                              <Form.Control id="password" ref={passwordRef} type="password" placeholder="Password" required/>
-                         </Form.Group>
-                         <Button disabled={loading} className="w-100" type="submit">Login</Button>
-                    </Form>
-                    <div className="w-100 text-center mt-3">
-                         <Link to="/forgot-password">Forgot Password?</Link>
+               <div className="w-100" style={{ maxWidth: '400px'}}>     
+                    <Card>
+                    <Card.Body>
+                         <h2 className="text-center mb-4">Login</h2>
+                         {error && <Alert variant="danger">{error}</Alert>}
+                         <Form onSubmit={handleSubmit}>
+                              <Form.Group id="email" className="mb-2">
+                                   <Form.Label>Email</Form.Label>
+                                   <Form.Control id="email" ref={emailRef} type="email" placeholder="Email" required/>
+                              </Form.Group>
+                              <Form.Group id="password" className="mb-4">
+                                   <Form.Label>Password</Form.Label>
+                                   <Form.Control id="password" ref={passwordRef} type="password" placeholder="Password" required/>
+                              </Form.Group>
+                              <Button disabled={loading} className="w-100" type="submit">Login</Button>
+                         </Form>
+                         <div className="w-100 text-center mt-3">
+                              <Link to="/forgot-password">Forgot Password?</Link>
+                         </div>
+                    </Card.Body>
+                    </Card>
+                    <div className="w-100 text-center mt-2">
+                         Need and account? <Link to="/signup">Sign up</Link>
                     </div>
-               </Card.Body>
-          </Card>
-          <div className="w-100 text-center mt-2">
-               Need and account? <Link to="/signup">Sign up</Link>
-          </div>
-          </div>
+               </div>
           </Container>
+          </>
      )
 }
 
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,5 +1,5 @@
 import React, {useRef, useState}from 'react';
-import { Form, Button, Card, Alert, Container } from 'react-bootstrap';
+import { Form, Button, Card, Alert, Container, Navbar, Nav } from 'react-bootstrap';
 import {useAuth} from '../../context/AuthContext';
 import { Link, useHistory } from 'react-router-dom';
 
@@ -34,34 +34,50 @@ const Signup = () => {
      }
 
      return (
-          <Container className="" style={{ minHeight: '100vh', paddingTop: '200px', paddingLeft: '460px'}}>
-          <div className="w-100" style={{ maxWidth: '400px'}}> 
-          <Card>
-               <Card.Body>
-                    <h2 className="text-center mb-4">Sign Up</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <Form onSubmit={handleSubmit}>
-                         <Form.Group id="email" className="mb-2">
-                              <Form.Label>Email</Form.Label>
-                              <Form.Control id="email" ref={emailRef} type="email" placeholder="Email" required/>
-                         </Form.Group>
-                         <Form.Group id="password" className="mb-2">
-                              <Form.Label>Password</Form.Label>
-                              <Form.Control id="password" ref={passwordRef} type="password" placeholder="Password" required/>
-                         </Form.Group>
-                         <Form.Group id="password-confirm" className="mb-4">
-                              <Form.Label>Password Confirmation</Form.Label>
-                              <Form.Control id="password" ref={passwordConfirmRef} type="password" placeholder="Password" required/>
-                         </Form.Group>
-                         <Button disabled={loading} className="w-100" type="submit">Sign Up</Button>
-                    </Form>
-               </Card.Body>
-          </Card>
-          <div className="w-100 text-center mt-2">
-               Already have an account? <Link to="/login">Login</Link>
-          </div>
-          </div>
+          <>
+          <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
+               <Container>
+                    <Navbar.Brand href="/">Elite-Division</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav" style={{justifyContent: 'end'}}>
+                    <Nav >
+                         <Nav.Link href="/signup">Sign up</Nav.Link>
+                         <Nav.Link eventKey={2} href="/login">
+                         Login
+                         </Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+               </Container>
+          </Navbar>
+          <Container className="" style={{ minHeight: '100vh', paddingTop: '150px', paddingLeft: '460px'}}>
+               <div className="w-100" style={{ maxWidth: '400px'}}> 
+                    <Card>
+                         <Card.Body>
+                              <h2 className="text-center mb-4">Sign Up</h2>
+                              {error && <Alert variant="danger">{error}</Alert>}
+                              <Form onSubmit={handleSubmit}>
+                                   <Form.Group id="email" className="mb-2">
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control id="email" ref={emailRef} type="email" placeholder="Email" required/>
+                                   </Form.Group>
+                                   <Form.Group id="password" className="mb-2">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control id="password" ref={passwordRef} type="password" placeholder="Password" required/>
+                                   </Form.Group>
+                                   <Form.Group id="password-confirm" className="mb-4">
+                                        <Form.Label>Password Confirmation</Form.Label>
+                                        <Form.Control id="password" ref={passwordConfirmRef} type="password" placeholder="Password" required/>
+                                   </Form.Group>
+                                   <Button disabled={loading} className="w-100" type="submit">Sign Up</Button>
+                              </Form>
+                         </Card.Body>
+                    </Card>
+                    <div className="w-100 text-center mt-2">
+                         Already have an account? <Link to="/login">Login</Link>
+                    </div>
+               </div>
           </Container>
+          </>
      )
 }
 
